87230132cf0bf5d0d2ef52bc8df8f8e31fc78637
Alex avellaneda <holalexdesarrolla@gmail.com>
2025-03-22 08:51:36 -0500

    Intento nuevo metodo 1

--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-  // Configuración específica para Cloudflare Pages
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000', '*.pages.dev', '*.workers.dev'],
-    },
-  },
 };
 
 export default nextConfig;
